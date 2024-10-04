422b75703d633ec39267d6449e9f85c9fe7f7318
Matheus <matheuszago134@gmail.com>
2024-10-04 13:31:40 -0300

    Add save and getByCpf methods in controller

--- msclientes/src/main/java/io/github/cursodsousa/msclientes/application/ClientesController.java
+++ msclientes/src/main/java/io/github/cursodsousa/msclientes/application/ClientesController.java
@@ -1,16 +1,48 @@
 package io.github.cursodsousa.msclientes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.cursodsousa.msclientes.application.representation.ClienteSaveRequest;
+import io.github.cursodsousa.msclientes.domain.Cliente;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
 public class ClientesController {
 
+    private final ClienteService service;
+
+    public ClientesController(ClienteService service) {
+        this.service = service;
+    }
+
     //Apenas para testar se entrou na app ou nn
     @GetMapping
     public String status(){
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> save(@RequestBody ClienteSaveRequest request){
+        var cliente = request.toModel();
+        service.save(cliente);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf){
+        var cliente = service.getByCpf(cpf);
+        if(cliente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(cliente);
     }
 }
---
+++ msclientes/src/main/java/io/github/cursodsousa/msclientes/application/representation/ClienteSaveRequest.java
@@ -0,0 +1,53 @@
+package io.github.cursodsousa.msclientes.application.representation;
+
+import io.github.cursodsousa.msclientes.domain.Cliente;
+
+import java.util.Objects;
+
+public class ClienteSaveRequest {
+
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Cliente toModel(){
+        return new Cliente(cpf, nome, idade);
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClienteSaveRequest that = (ClienteSaveRequest) o;
+        return Objects.equals(cpf, that.cpf) && Objects.equals(nome, that.nome) && Objects.equals(idade, that.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf, nome, idade);
+    }
+}
