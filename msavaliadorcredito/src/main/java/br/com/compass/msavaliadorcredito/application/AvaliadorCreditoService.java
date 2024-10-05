89df3db0c5edee62dabee25a503c64770068d541
Matheus <matheuszago134@gmail.com>
2024-10-05 13:49:13 -0300

    Add comunication between cartao and avaliador

--- msavaliadorcredito/src/main/java/br/com/compass/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/br/com/compass/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,28 +1,36 @@
 package br.com.compass.msavaliadorcredito.application;
 
+import br.com.compass.msavaliadorcredito.domain.model.CartaoCliente;
 import br.com.compass.msavaliadorcredito.domain.model.DadosCliente;
 import br.com.compass.msavaliadorcredito.domain.model.SituacaoCliente;
+import br.com.compass.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import br.com.compass.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clientesClient;
+    private final CartoesResourceClient cartoesClient;
 
-    public AvaliadorCreditoService(ClienteResourceClient clientesClient) {
+    public AvaliadorCreditoService(ClienteResourceClient clientesClient, CartoesResourceClient cartoesClient) {
         this.clientesClient = clientesClient;
+        this.cartoesClient = cartoesClient;
     }
 
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         //ObterDadosClientes - MSCLIENTES
         ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+        //ObterDadosCartoes - MSCARTOES
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesPorCpf(cpf);
+
         return SituacaoCliente.builder()
                 .cliente(dadosClienteResponse.getBody())
-                //Adicionar cartoes dps
+                .cartoes(cartoesResponse.getBody())
                 .build();
-        //ObterDadosCartoes - MSCARTOES
     }
 
 }
---
+++ msavaliadorcredito/src/main/java/br/com/compass/msavaliadorcredito/infra/clients/CartoesResourceClient.java
@@ -0,0 +1,17 @@
+package br.com.compass.msavaliadorcredito.infra.clients;
+
+import br.com.compass.msavaliadorcredito.domain.model.CartaoCliente;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartoesResourceClient {
+
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<CartaoCliente>> getCartoesPorCpf(@RequestParam("cpf") String cpf);
+
+}
