de05bd2138bee1897c9c713bf2bd72581bb64d48
Matheus <matheuszago134@gmail.com>
2024-10-07 12:48:24 -0300

    Add subscriber for emissao-cartao

---
+++ mscartoes/src/main/java/br/com/compass/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -0,0 +1,16 @@
+package br.com.compass.mscartoes.infra.mqueue;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmissaoCartaoSubscriber {
+
+    //Está escutando essa fila, para qnd ela for chamadaa
+    @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
+    public void receberSolicitacaoEmissao(@Payload String payload){
+        System.out.println(payload);
+    }
+
+}
